446ca60905a5112108dc62744fc99c9ce3cd9d29
Phạm Hà Nam 20069861 <98084540+pnam55588@users.noreply.github.com>
2025-07-17 22:51:03 +0700

    chore: cấu hình ESLint và Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            "prettier/prettier": "error",
+        },
+    },
 ];
 
 export default eslintConfig;
